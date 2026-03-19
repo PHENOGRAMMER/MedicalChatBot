82e905a4af92fd50765295c7811f1f35c326a338
PHENOGRAMMER <aryanbalani99@gmail.com>
2026-03-19 16:03:31 +0530

    Requirements Added.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="MedicalChatBot",
+    version="0.1.0",
+    author="Aryan Balani",
+    author_email="aryanbalani99@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
